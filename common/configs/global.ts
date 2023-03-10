04026cffec2a50e54d91516e91d27b1703050124
yiminfe <yiminfe@163.com>
2023-03-10 08:24:43 +0800

    修复bug

--- common/configs/global.ts
+++ common/configs/global.ts
@@ -1,5 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-export const swFile = isProd ? 'https://static.yiminfe.com/nuxt-ssr/sw-prod.js' : '/sw-dev.js'
+export const swFile = isProd ? '/sw-prod.js' : '/sw-dev.js'
 
 export const apiUrl = 'https://nuxt.yiminfe.com'
+s
---
+++ dist/public/_nuxt/auth.75d8dc94.js
@@ -0,0 +1,1 @@
+import{T as a,P as r,U as t}from"./entry.abd005a0.js";const e=a(((a,e)=>{if(1!==r().value)return t(`/login?redirect=${a.path}`)}));export{e as default};
